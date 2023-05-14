2c49e4b344db8e6e682ebc5ea72eddbab42cd5f6
Terry-Rossi <terryrossi1@gmail.com>
2023-05-13 18:51:59 -0700

    Modified for preventDefault on enter key and cleaner design

--- js/script.js
+++ js/script.js
@@ -1,36 +1,45 @@
 // const toDoListApp = (function newItem() {
+$(document).ready(function () {
+	$(window).keydown(function (event) {
+		if (event.keyCode == 13) {
+			event.preventDefault();
+			return false;
+		}
+	});
+});
+
 function newItem() {
-	const list = $("#list");
-	const inputValue = $("#input").val();
+	const list = $('#list');
+	const inputValue = $('#input').val();
 	const li = $(`<li>${inputValue}</li>`);
 
 	if (inputValue.length < 1) {
-		alert("Please enter an Item to add on the list");
+		alert('Please enter an Item to add on the list');
 	} else {
 		list.append(li);
-		$("#input").val("");
+		$('#input').val('');
 	}
 	// Crossing out an Item from the list of items
-	function crossOut() {
-		li.toggleClass("strike");
-	}
-	// li.on("click", crossOut);
-	li.on("dblclick", function crossOut() {
-		li.toggleClass("strike");
+	// function crossOut() {
+	// 	li.toggleClass('strike');
+	// }
+	// li.on("dblclick", crossOut);
+	li.on('click', function crossOut() {
+		li.toggleClass('strike');
 	});
 
 	// ADDING DELETE BUTTON 'X'
 	const crossOutButton = $(`<crossOutButton>X</crossOutButton>`);
 	li.append(crossOutButton);
 
-	crossOutButton.on("click", deleteListedItem);
+	crossOutButton.on('click', deleteListedItem);
 
 	function deleteListedItem() {
-		li.addClass("delete");
+		li.addClass('delete');
 	}
 
 	// MAKE LIST RE-ORDERABLE
-	$("#list").sortable();
+	$('#list').sortable();
 	// return {
 	// 	newItem,
 }
